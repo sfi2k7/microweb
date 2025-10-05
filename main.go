fbabfe565a7326d849a824f38cb1704cb6702148
Faisal Iqbal <sfi2k6@outlook.com>
2025-10-06 00:53:20 +0400

    context rename

--- main.go
+++ main.go
@@ -11,21 +11,21 @@ import (
 	"syscall"
 )
 
-type microcontext struct {
+type Context struct {
 	R      *http.Request
 	W      http.ResponseWriter
 	Method string
 }
 
-func (tc *microcontext) Json(v any) error {
+func (tc *Context) Json(v any) error {
 	return json.NewEncoder(tc.W).Encode(v)
 }
 
-func (tc *microcontext) Query(key string) string {
+func (tc *Context) Query(key string) string {
 	return tc.R.URL.Query().Get(key)
 }
 
-func (tc *microcontext) Parse(target any) error {
+func (tc *Context) Parse(target any) error {
 	body, err := io.ReadAll(tc.R.Body)
 	if err != nil {
 		return err
@@ -35,7 +35,7 @@ func (tc *microcontext) Parse(target any) error {
 	return json.Unmarshal(body, target)
 }
 
-func (tc *microcontext) Body() ([]byte, error) {
+func (tc *Context) Body() ([]byte, error) {
 	body, err := io.ReadAll(tc.R.Body)
 	if err != nil {
 		return nil, err
@@ -44,15 +44,15 @@ func (tc *microcontext) Body() ([]byte, error) {
 	return body, nil
 }
 
-func (tc *microcontext) String(str string) error {
+func (tc *Context) String(str string) error {
 	tc.W.WriteHeader(http.StatusOK)
 	_, err := fmt.Fprintf(tc.W, "%s", str)
 	return err
 }
 
-func middle(fn func(*microcontext)) http.HandlerFunc {
+func middle(fn func(*Context)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := &microcontext{R: r, W: w, Method: r.Method}
+		ctx := &Context{R: r, W: w, Method: r.Method}
 		fn(ctx)
 	})
 }
@@ -91,31 +91,31 @@ func (mw *MicroWeb) StaticWithPrefix(prefix, path string) {
 	http.Handle(prefix, http.StripPrefix(prefix, http.FileServer(http.Dir(path))))
 }
 
-func (mw *MicroWeb) Get(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Get(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Post(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Post(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Put(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Put(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Delete(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Delete(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Head(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Head(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Options(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Options(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
-func (mw *MicroWeb) Patch(path string, handler func(*microcontext)) {
+func (mw *MicroWeb) Patch(path string, handler func(*Context)) {
 	http.HandleFunc(path, middle(handler))
 }
 
